3a1a4408f3fecaae21f89c0054b100e4a228b9c7
shameemfrhn <shameemfarhan.sfk@gmail.com>
2023-09-26 23:44:30 +0530

    1st

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import StyledComponent from './StyledComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App with CSS Styling</h1>
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/StyledComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StyledComponent.css';
+
+function StyledComponent() {
+  return (
+    <div className="styled-component">
+      <h1 className="title">Styled Component</h1>
+      <p className="description">This component is styled using CSS.</p>
+    </div>
+  );
+}
+
+export default StyledComponent;
